5e209213bd6ecfda717ae68e09f576f21fd58e8d
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2021-08-16 11:23:01 +0200

    Added actions, api and reducers
    Added actions, api and reducers to file structure, also started JSX syntaxes for the site layout

---
+++ client/src/actions/posts.js
@@ -0,0 +1,18 @@
+import * as api from '../api';
+
+//action creators
+
+export const getPosts = () => async (dispatch) => {
+    try{
+        const { data } = await api.fetchPosts();
+        
+        dispatch({type: 'FETCH_ALL', payload: data})
+    }catch(err){
+        console.log(err.message);
+    }
+
+    
+    const action = { type: 'FETCH_ALL', payload: []}
+
+    dispatch(action);
+}
