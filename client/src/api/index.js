5d7ea29e68481ddea2a98b2529d7d59b1510843b
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2021-08-16 23:20:17 +0200

    Changes to clientside files
    Changes to clientside files done, now able to send data to Database with success

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,4 +4,9 @@ import axios from 'axios';
 const url = 'http://localhost:2000/posts';
 
 
+<<<<<<< Updated upstream
 export const fetchPosts = () => axios.get(url);
+=======
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+>>>>>>> Stashed changes
