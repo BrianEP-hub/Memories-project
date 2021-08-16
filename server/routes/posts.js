850d567fb98ba86997044a3634d48dc19262ab16
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2021-08-16 23:16:49 +0200

    updated files
    Updated all server files in directory, server now up and running and able to take actions of API calls

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
-import { getPosts, createPosts } from '../controller/posts.js'
-
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controller/posts.js';
 
 const router = express.Router();
 
-
-router.get('/', getPosts)
-
-router.post('/', createPosts)
-
+router.get('/', getPosts);
+router.post('/', createPost);
+router.get('/:id', getPost);
+router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 
 export default router;
