88eae1c7dc0882f709565b7f11c3edf9be736050
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2021-08-17 21:09:11 +0200

    updated clientside files
    Updated all clientside files, now showing posts from API call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -9,4 +9,10 @@ export const fetchPosts = () => axios.get(url);
 =======
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
 >>>>>>> Stashed changes
