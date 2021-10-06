ae98afd5130ee6fb8302a52b5533e886746821b6
BrianEP-hub <54816472+BrianEP-hub@users.noreply.github.com>
2021-10-06 09:05:27 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,26 +9,6 @@ import {  Navbar, Home, Auth } from './components';
 const App = () => {
    
     return (
-<<<<<<< Updated upstream
-        <Container maxwidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                    <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid classes={classes.mainContainer} container justify="space-between" alignItems="strech" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
-=======
         <BrowserRouter>
              <Container maxwidth="lg">
                     <Navbar />   
@@ -39,7 +19,6 @@ const App = () => {
              </Container>
         </BrowserRouter>
         
->>>>>>> Stashed changes
     )
 }
 
